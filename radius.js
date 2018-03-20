1550713338263f74f30ce7d3f5db2446a5367028
Rizel Bobb-Semple <rizel.bobbsemple137@gmail.com>
2018-03-20 00:41:24 -0400

    Update radius.js

--- radius.js
+++ radius.js
@@ -1,7 +1,7 @@
 var circleArea = function(radius) {
   var area= radius* radius* Math.PI
   var roundingArea=parseFloat(area.toFixed(2))
-  if(isNaN(radius) || radius <=0 ){
+  if(isNaN(radius) || radius <==0 ){
     return false
   }return roundingArea
   }
